e5c4e3225d9d397bdcd6d2bee37057c88a2a5ed3
Alvaro E. <alvesuiz@gmail.com>
2024-10-08 13:16:55 +0200

    message: Ejercicios con variables

---
+++ dwec_misejemplos/js/ejercicio02.js
@@ -0,0 +1,13 @@
+let x, y, z
+x = 2
+y = 3
+z = 5
+
+const num = x+z+y
+console.log(num)
+let lastname, lastName;
+lastName = "Alvaro";
+lastname = "Estacio";
+
+fullname = lastname + lastName
+console.log(fullname)
