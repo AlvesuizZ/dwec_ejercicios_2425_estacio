e52d793469cd1ee8add9ef24107cb574ca6838e6
Alvaro E. <alvesuiz@gmail.com>
2024-10-09 14:21:59 +0200

    ejercicio medias terminado

--- dwec_ejercicios_t01/js/ejercicio05a.js
+++ dwec_ejercicios_t01/js/ejercicio05a.js
@@ -1,22 +1,25 @@
-x = parseInt(prompt("Dime un primer entero: "))
-x2 = parseInt(prompt("Dime un segundo entero: "))
-x3 = parseInt(prompt("Dime un tercer entero: "))
-x4 = parseInt(prompt("Dime un cuarto entero: "))
-x5 = parseInt(prompt("Dime un quinto entero: "))
-const total = (x + x2 + x3 + x4 + x5)
-const media = total / 5;
+let x1 = parseInt(prompt("Dime un primer entero: "));
+let x2 = parseInt(prompt("Dime un segundo entero: "));
+let x3 = parseInt(prompt("Dime un tercer entero: "));
+let x4 = parseInt(prompt("Dime un cuarto entero: "));
+let x5 = parseInt(prompt("Dime un quinto entero: "));
+let total = x1 + x2 + x3 + x4 + x5;
+let media = total / 5;
 
-if(x > media){
-    alert('El numero ' + x + " es mayor a la media")
-}else if(x2 > media){
-    alert('El numero ' + x2 + " es mayor a la media")
-}else if(x3 > media){
-    alert('El numero ' + x3 + " es mayor a la media")
-}else if(x4 > media){
-    alert('El numero ' + x4 + " es mayor a la media")
-}else if(x5 > media){
-    alert('El numero ' + x5 + " es mayor a la media")
-    
-}else{
-    alert("Ningun numero es mayor que la media")
+if (x1 > media) {
+  alert("El numero " + x1 + " es mayor a la media");
+}
+if (x2 > media) {
+  alert("El numero " + x2 + " es mayor a la media");
+}
+if (x3 > media) {
+  alert("El numero " + x3 + " es mayor a la media");
+}
+if (x4 > media) {
+  alert("El numero " + x4 + " es mayor a la media");
+}
+if (x5 > media) {
+  alert("El numero " + x5 + " es mayor a la media");
+} else {
+  alert("Ningun numero es mayor que la media");
 }
