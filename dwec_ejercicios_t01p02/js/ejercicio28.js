a983372ce0bd05f17de17e3096a17348800f5c95
Alvaro E. <alvesuiz@gmail.com>
2024-10-22 13:34:24 +0200

    Terminando Ejercicio fechas

---
+++ dwec_ejercicios_t01p02/js/ejercicio28.js
@@ -0,0 +1,57 @@
+
+fecha = prompt(
+    "Introduce una fecha (formato: DD-MM-YYYY, DD/MM/YYYY o DD MM YYYY):"
+);
+let sign = "";
+let seguir = true;
+
+if (fecha.includes("/")) {
+    sign = "/";
+} else if (fecha.includes("-")) {
+    sign = "-";
+} else if (fecha.includes(" ")) {
+    sign = " ";
+} else {
+    alert("Fecha no valida debido al formato incorrecto");
+    seguir = false;
+}
+
+if (seguir) {
+    let fechaParts = fecha.split(sign);
+
+    if (fechaParts.length !== 3) {
+    alert("Formato de fecha inválido");
+    } else {
+    let hoy = new Date();
+    let year2;
+    let day = parseInt(fechaParts[0]);
+    let month = parseInt(fechaParts[1]);
+    let year = parseInt(fechaParts[2]);
+    let cumple = new Date(year, (month-1), day);
+    let edad = hoy.getFullYear() - year;
+    if(hoy.getMonth() == cumple.getMonth() && hoy.getDate() == cumple.getDate() ){
+        console.log("Felicidades hoy es tu cumpleaños, hoy cumples: " + edad)
+    }
+    if(hoy.getMonth() > cumple.getMonth()){
+        year2 = hoy.getFullYear() + 1
+    }else if(hoy.getMonth() < cumple.getMonth()){
+        year2 = hoy.getFullYear()
+    }
+    if (hoy.getMonth() == cumple.getMonth()){
+        if(hoy.getDate() < cumple.getDate()){
+            year2 = hoy.getFullYear() + 1
+        }else if(hoy.getDate() > cumple.getDate()){
+            year2 = hoy.getFullYear()
+        }
+    }
+
+    let fechaAyda = new Date(year2, (month-1), day)
+    //let fechaAyda2 = new Date(hoy.getFullYear(), month-1, day)
+    let calc = (hoy - fechaAyda)
+    let total = Math.round(calc/(1000*60*60*24))
+
+    console.log(total)
+}
+
+
+}
