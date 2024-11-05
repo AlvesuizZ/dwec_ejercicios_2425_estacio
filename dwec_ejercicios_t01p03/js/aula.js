93617979b2a98c5dc04066e2b8da75707ff5cd3b
Alvaro Estacio Ruiz <alvesuiz@gmail.com>
2024-11-05 12:46:29 +0100

    Seguimiento del segundo ejercicia de las aulas

--- dwec_ejercicios_t01p03/js/aula.js
+++ dwec_ejercicios_t01p03/js/aula.js
@@ -1,33 +1,95 @@
-function Aula(nAlumnos, id, descripcion){
+function Aula(nAlumnos, id, descripcion) {
     this._nAlmunos = nAlumnos;
     this._id = id;
     this._descripcion = descripcion;
     this.alumnos = [0];
 
-    this.pedirDatosAlumno = (dni, nombre, fechaNacimiento, nota1, nota2, nota3, sexo) =>{
+    this.pedirDatosAlumno = () => {
         let exit = false
         let nombre = prompt("Dime tu nombre: ")
-        let  dni = prompt("Dime tu dni: ").toUpperCase();
+        let dni = prompt("Dime tu dni: ").toUpperCase();
         let fechaNacimiento = prompt("Dime tu fecha de nacimiento: ")
         let nota1 = prompt("Dime tu nota de la primera evaluacion: ")
         let nota2 = prompt("Dime tu nota de la segunda evaluacion: ")
         let nota3 = prompt("Dime tu nota de la tercera evaluacion: ")
-        let sexo = prompt("Dime tu sexo: ")
-        
+        let sexo = prompt("Dime tu sexo: ").toUpperCase();
+
+        do {
+            if (isNaN(nombre)) {
+                exit = true;
+            } else {
+                console.log("El nombre no es valido porfavor introduzcalo otra vez")
+                nombre = prompt("Dime tu nombre: ")
+            }
+        } while (!exit);
+        exit = false;
         do {
             if (this.validarDNI(dni) == false) {
                 console.log("El dni pasado por consola no es valido")
                 dni = prompt("Dime tu dni: ").toUpperCase();
-                break;
+            }else{
+                exit = true;
             }
+        } while (!exit);
 
+        exit = false
+        //validacion de la nota 1
+        do {
+            if (!this.validarNota(nota1)) {
+                console.log("La nota no es valido porfavor agregelo otra vez")
+                nota1 = prompt("Dime tu nota de la primera evaluacion: ")
+            } else {
+                exit = true
+            }
+        } while (!exit);
+
+        exit = false
+        //validacion de la nota 2
+        do {
+            if (!this.validarNota(nota2)) {
+                console.log("La nota no es valido porfavor agregelo otra vez")
+                nota1 = prompt("Dime tu nota de la primera evaluacion: ")
+            } else {
+                exit = true
+            }
+        } while (!exit);
+
+        exit = false
+        //validacion de la nota 3
+        do {
+            if (!this.validarNota(nota3)) {
+                console.log("La nota no es valido porfavor agregelo otra vez")
+                nota1 = prompt("Dime tu nota de la primera evaluacion: ")
+            } else {
+                exit = true
+            }
+        } while (!exit);
+
+        exit = false
+
+        do {
+            if (!this.validarGenero(sexo)) {
+                console.log("El genero introducido no es valido")
+                sexo = prompt("Dime tu sexo: ").toUpperCase();
+            } else {
+                exit = true;
+            }
+        } while (!exit);
+        exit = false;
+        do {
+            if (!validarFecha(fechaNacimiento)) {
+                console.log("la fecha no es valida por favor introducela de nuevo")
+                fechaNacimiento = prompt("Dime tu fecha de nacimiento(DD/MM/YYYY  DD-MM-YYYY): ")
+            } else {
+                exit = true
+            }
         } while (!exit);
     }
+
 
     this.validarDNI = (dni) => {
 
         if (!/^\d{8}[A-Z]$/.test(dni)) {
-            resultado.textContent = "Formato de DNI no válido.";
             return false;
         }
 
@@ -43,4 +105,70 @@ function Aula(nAlumnos, id, descripcion){
             return false
         }
     }
+
+    this.validarNota = (nota) => {
+
+        if (isNaN(nota)) {
+            console.log("La nota debera ser un numero")
+            return false
+        } else if (nota < 0) {
+            console.log("La nota no puede ser negativa")
+            return false
+        } else if (nota > 10) {
+            console.log("La nota no puede ser mayor de 10")
+            return false
+        } else {
+            return true
+        }
+    }
+
+    this.validarGenero = (sexo) => {
+        if (sexo === "H" || sexo === "M" || sexo === "O") {
+            return true
+        } else {
+            return false
+        }
+    }
+    this.dateValidator = (date) => {
+        const pattern = /^(0[1-9]|[12][0-9]|3[01])[-\/](0[1-9]|1[0-2])[-\/](\d{2}|\d{4})$/;
+        return pattern.test(date)
+    }
+    this.esBisiesto = (year) => {
+        return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+    }
+    
+    this.validarFecha = (date) => {
+        let sign = "";
+        let fechaParts;
+        if (dateValidator(date)) {
+            if (date.includes(" ")) {
+                return false;
+            }
+            if (date.includes("/")) {
+                sign = "/";
+            } else {
+                sign = "-";
+            }
+            let fechaParts = date.split(sign);
+            let day = parseInt(fechaParts[0]);
+            let month = parseInt(fechaParts[1]);
+            let year = parseInt(fechaParts[2]);
+            if (esBisiesto(year) && month === 2 && day <= 29) {
+                console.log("La fecha es valida");
+                return true;
+            } else if (month === 2 && day > 28) {
+                console.log("La fecha no es valida");
+                return false;
+            } else {
+                console.log("La fecha introducida es valida");
+                return true;
+            }
+        } else {
+            console.log("La fecha no es valida");
+            return false;
+        }
+    }
+    
 }
+
+
--- dwec_ejercicios_t01p03/js/ejercicio01_app.js
+++ dwec_ejercicios_t01p03/js/ejercicio01_app.js
@@ -1,16 +1,19 @@
 console.log("T0X - Ejercicio 01");
 
 function funcionPrueba1() {
-    let alumno1 = new Alumno('12345678', 'Sergio Estacio', '2008-12-15', 7.8, 6.5, 8.3, 'H');
-    console.log(alumno1.mostrarInformacion());  
+    // let alumno1 = new Alumno('12345678', 'Sergio Estacio', '2008-12-15', 7.8, 6.5, 8.3, 'H');
+    // console.log(alumno1.mostrarInformacion());  
 
-    alumno1.cambiarNotas(9.1, 7.4, 8.6);  
-    console.log(alumno1.mostrarInformacion());  
+    // alumno1.cambiarNotas(9.1, 7.4, 8.6);  
+    // console.log(alumno1.mostrarInformacion());  
 
-    console.log(alumno1.estaAprobado());  
+    // console.log(alumno1.estaAprobado());  
 
-    let alumno2 = new Alumno('87654321', 'Alvaro Estacio', '2004-03-10', 8.5, 7.9, 9.2, 'H');
-    console.log(alumno1.comparar(alumno2)); 
+    // let alumno2 = new Alumno('87654321', 'Alvaro Estacio', '2004-03-10', 8.5, 7.9, 9.2, 'H');
+    // console.log(alumno1.comparar(alumno2)); 
+
+    const aula = new Aula(1, 1, "Descripcion");
+    aula.pedirDatosAlumno();
 }
 funcionPrueba1();
 
